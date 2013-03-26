dc0e064ac9e3e6fe8527528a303718a6fbbccece
IVCKal411 <yogesh.kalaskar@infovision.net>
2013-03-26 10:09:13 -0500

    test

---
+++ SampleGitTests/SampleGitTests.h
@@ -0,0 +1,13 @@
+//
+//  SampleGitTests.h
+//  SampleGitTests
+//
+//  Created by IVC_Yogesh on 3/26/13.
+//  Copyright (c) 2013 IVCBAR-M022. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SampleGitTests : SenTestCase
+
+@end
---
+++ SampleGitTests/SampleGitTests.m
@@ -0,0 +1,32 @@
+//
+//  SampleGitTests.m
+//  SampleGitTests
+//
+//  Created by IVC_Yogesh on 3/26/13.
+//  Copyright (c) 2013 IVCBAR-M022. All rights reserved.
+//
+
+#import "SampleGitTests.h"
+
+@implementation SampleGitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SampleGitTests");
+}
+
+@end
